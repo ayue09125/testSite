83a0bb778e41e181e716501255051e9ea2e514df
A <ayue09125@gmail.com>
2025-04-21 18:06:33 -0700

    --

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const apiKey = process.env.GEMINI_API_KEY;
-const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
+import generateText from "./api/generate.js"
 
 const createMessageElement = function(content, classes) {
   const div = document.createElement("div");
@@ -47,36 +46,5 @@ document.querySelector(".material-symbols-rounded").addEventListener("click", fu
 })
 
 
-// generate bot's response with Gemini API
-async function generateText(prompt) {    
-    const data = {
-        contents: [{
-            parts: [{ text: prompt }]
-        }]
-    };
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-
-        if (!response.ok) {
-            const error = await response.json();
-            console.error("API Error:", error);
-            return null;
-        }
-
-        const result = await response.json();
-        const responseText = result.candidates[0].content.parts[0].text.trim();
-        console.log("API Response:", responseText);
-        return responseText;
-    } catch (error) {
-        console.error("Fetch Error:", error);
-        return null;
-    }
-}
 
